64c48abf28c09205b246e6281ad7d4f411983b74
enesozudogru <enesozudogruu@gmail.com>
2026-01-02 18:54:50 +0300

    feat: Add device security types enumeration and integrate into main app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_device_security/flutter_device_security.dart';
+import 'package:flutter_device_security/src/models/device_security.dart';
+import 'package:flutter_device_security_example/enums/device_security_types.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,22 +17,81 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final String _platformVersion = 'Unknown';
-  final _flutterDeviceSecurityPlugin = FlutterDeviceSecurity();
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  Future<void> initPlatformState() async {
-    final isDeviceSecure = await _flutterDeviceSecurityPlugin.checkDeviceSecurity();
-    // isDeviceSecure.
+  final ValueNotifier<DeviceSecurity?> deviceSecurityNotifier = ValueNotifier<DeviceSecurity?>(null);
+  final ValueNotifier<bool> loadingNotifier = ValueNotifier<bool>(false);
+  final flutterDeviceSecurity = FlutterDeviceSecurity();
+  Future<void> checkDeviceSecurity() async {
+    try {
+      loadingNotifier.value = true;
+      deviceSecurityNotifier.value = await flutterDeviceSecurity.checkDeviceSecurity();
+    } catch (e) {
+      debugPrint('Device security check error: $e');
+    } finally {
+      loadingNotifier.value = false;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(appBar: AppBar(title: const Text('Plugin example app')), body: Center(child: Text('Running on: $_platformVersion\n'))));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Device Security')),
+      body: ValueListenableBuilder(
+        valueListenable: loadingNotifier,
+        builder: (context, isLoading, child) {
+          if (isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ValueListenableBuilder<DeviceSecurity?>(
+                valueListenable: deviceSecurityNotifier,
+                builder: (context, deviceSecurity, child) {
+                  if (deviceSecurity != null) {
+                    return ListView(
+                      shrinkWrap: true,
+                      children: [
+                        DeviceSecurityTile(type: DeviceSecurityTypes.passcode, value: deviceSecurity.passcode),
+                        DeviceSecurityTile(type: DeviceSecurityTypes.version, value: deviceSecurity.version),
+                        if (deviceSecurity is AndroidDeviceSecurity) ...[
+                          DeviceSecurityTile(type: DeviceSecurityTypes.biometric, value: deviceSecurity.biometric),
+                          DeviceSecurityTile(type: DeviceSecurityTypes.usbDebugging, value: !(deviceSecurity.usbDebugging ?? false)),
+                        ],
+                      ],
+                    );
+                  }
+                  return const SizedBox.shrink();
+                },
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.25),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: ElevatedButton(
+                  onPressed: checkDeviceSecurity,
+                  style: ElevatedButton.styleFrom(padding: const EdgeInsets.symmetric(vertical: 16), backgroundColor: Colors.blueAccent, foregroundColor: Colors.white),
+                  child: const Text('Start Scan'),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DeviceSecurityTile extends StatelessWidget {
+  const DeviceSecurityTile({super.key, required this.type, this.value});
+
+  final DeviceSecurityTypes type;
+  final bool? value;
+
+  @override
+  Widget build(BuildContext context) {
+    if (!type.show) {
+      return const SizedBox.shrink();
+    }
+    return ListTile(leading: Icon(type.icon, color: value == true ? Colors.green : Colors.red), title: Text(type.label, style: TextStyle(color: value == true ? Colors.green : Colors.red)));
   }
 }
